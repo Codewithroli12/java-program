993c71cca41209d74b7da70161d902b046b77dd7
Rolikushwaha <rolikushwaha848880@gmail.com>
2026-06-13 22:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (string &word : words) {
+            int sum = 0;
+
+            for (char c : word) {
+                sum = (sum + weights[c - 'a']) % 26;
+            }
+
+            // 0 -> 'z', 1 -> 'y', ..., 25 -> 'a'
+            ans.push_back('a' + (25 - sum));
+        }
+
+        return ans;
+    }
+};
